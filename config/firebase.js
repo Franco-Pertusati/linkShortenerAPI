5e7e0a2c6fb4382bd78018e652241c87f33eae7e
Franco Pertusati <pertusatifranco836@gmail.com>
2024-11-06 12:57:42 -0300

    Prueba con secret file

--- config/firebase.js
+++ config/firebase.js
@@ -1,20 +1,11 @@
 const admin = require("firebase-admin");
+const path = require("path");
 
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-};
+// Ruta al archivo JSON de credenciales de Firebase
+const serviceAccountPath = path.resolve(__dirname, "/etc/secrets/linktun-558d6-firebase-adminsdk-aym5j-26cbeaa820");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(require(serviceAccountPath)),
 });
 
 const db = admin.firestore();
