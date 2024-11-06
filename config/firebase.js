fd15d1c1e0b76ceff53187413eab964fd985d89a
Franco Pertusati <pertusatifranco836@gmail.com>
2024-11-06 08:53:29 -0300

    cambiada la config de firebase para que lea las variables de entorno

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,6 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../linktun-558d6-firebase-adminsdk-aym5j-26cbeaa820.json');
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
