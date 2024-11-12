298eaf0db46d3993e0e525998f613fd4213a7794
Franco Pertusati <pertusatifranco836@gmail.com>
2024-11-11 21:30:16 -0300

    funcionalidad de favicon

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,27 +1,38 @@
-const { saveUrl, getUrl } = require("../services/urlServices");
+const axios = require('axios');
+const { saveUrl, getUrl } = require('../services/urlServices');
+
 const toBase62 = (num) => {
-  const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  let result = "";
+  const chars =
+    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let result = '';
   while (num > 0) {
     result = chars[num % 62] + result;
     num = Math.floor(num / 62);
   }
-  return result.padStart(6, "0");
+  return result.padStart(6, '0');
 };
 
 const shortenUrl = async (req, res) => {
   const { originalUrl } = req.body;
   if (!originalUrl) {
-    return res.status(400).json({ error: "URL is required" });
+    return res.status(400).json({ error: 'URL is required' });
   }
 
   const shortCode = toBase62(Date.now());
+
   try {
     await saveUrl(shortCode, originalUrl);
-    res.json({ shortUrl: `${req.protocol}://${req.get("host")}/${shortCode}` });
+
+    const faviconUrl = `https://www.google.com/s2/favicons?sz=64&domain_url=${originalUrl}`;
+
+    res.json({
+      originalUrl,
+      shortUrl: `${req.protocol}://${req.get('host')}/${shortCode}`,
+      favicon: faviconUrl,
+    });
   } catch (error) {
-    console.error("Error saving URL:", error);
-    res.status(500).json({ error: "Failed to shorten URL" });
+    console.error('Error saving URL:', error);
+    res.status(500).json({ error: 'Failed to shorten URL' });
   }
 };
 
@@ -30,12 +41,12 @@ const redirectUrl = async (req, res) => {
   try {
     const originalUrl = await getUrl(code);
     if (!originalUrl) {
-      return res.status(404).json({ error: "URL not found" });
+      return res.status(404).json({ error: 'URL not found' });
     }
     res.redirect(originalUrl);
   } catch (error) {
-    console.error("Error retrieving URL:", error);
-    res.status(500).json({ error: "Failed to retrieve URL" });
+    console.error('Error retrieving URL:', error);
+    res.status(500).json({ error: 'Failed to retrieve URL' });
   }
 };
 
