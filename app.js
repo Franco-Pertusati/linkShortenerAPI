b8d209a88818f04c598bc3e9f2a5da1a6263c869
Franco Pertusati <pertusatifranco836@gmail.com>
2024-11-21 13:39:00 -0300

    Agregada lista de dominios permitidos

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const cors = require('cors');
 const urlRoutes = require('./routes/urlRoutes');
 
 const app = express();
+const allowedOrigins = [
+  /https:\/\/franco-pertusati\.github\.io/,
+  'http://localhost:4200'
+];
 
-app.use(cors({ origin: 'https://franco-pertusati.github.io' }));
-app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.some(pattern => pattern.test(origin))) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json());
 app.use('/', urlRoutes);
 
