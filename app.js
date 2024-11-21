8a760ed5275974e1fb4bf1c357a39b98c2fc6950
Franco Pertusati <pertusatifranco836@gmail.com>
2024-11-21 12:57:21 -0300

    Correccion de error de cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const urlRoutes = require('./routes/urlRoutes');
 
 const app = express();
 
-app.use(cors({ origin: 'https://franco-pertusati.github.io/Linktun/home' }));
+app.use(cors({ origin: 'https://franco-pertusati.github.io' }));
 app.use(express.json());
 app.use('/', urlRoutes);
 
